adb0bb98bf441162e6d30f158bc44b79352e2ca0
Luciano Ramalho <luciano@ramalho.org>
2016-02-22 07:33:10 -0300

    lista de palavras gerada

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# palavras
-Lista de palavras do português brasileiro
+# Palavras PT-BR
+
+O arquivo `palavras.txt` neste repositório contém mais de 320.000 palavras do idioma português brasileiro.
+
+A fonte principal é a lista de palavras do corretor ortográfico LibreOffice, obtida aqui:
+
+https://cgit.freedesktop.org/libreoffice/dictionaries/plain/pt_BR/pt_BR.dic
+
+O arquivo original foi processado pelo programa `converter.py` para:
+
+1. converter a codificação de Latin-1 para UTF-8
+
+2. remover os códigos alfabéticos apensados a algumas palavras, após uma `/`
+
+3. acrescentar em linhas separadas as palavras que formam termos compostos (ex. casa-forte)
+
+4. reordenar tudo alfabeticamente
+
+O motivo do passo 3 é que a palavra "casa" não tem uma entrada individual no arquivo original! Se alguém souber o motivo, cadastre um *issue* explicando em que parte do código-fonte do corretor ortográfico do LibreOffice está a informação de que "casa" é uma palavra.
---
+++ converter.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import unicodedata
+
+
+ENTRADA = 'pt_BR.dic'
+SAIDA = 'palavras.txt'
+
+
+def normalizar(txt):
+    """Remove acentos e transforma 'A' ou 'ª' em 'a'."""
+    norm_txt = unicodedata.normalize('NFKD', txt).lower()
+    shaved = ''.join(c for c in norm_txt
+                     if not unicodedata.combining(c))
+    return unicodedata.normalize('NFC', shaved)
+
+
+def chave(txt):
+    """Manter palavras acentuadas após as não acentuadas"""
+    codigos = tuple(ord(c) for c in txt)
+    return (normalizar(txt), codigos)
+
+
+palavras = set()
+with open(ENTRADA, encoding='latin-1') as entrada:
+    for i, linha in enumerate(entrada):
+        if i == 0:
+            continue  # ignorar primeira linha (contagem)
+        linha = linha.strip()
+        linha = linha.split('/')[0]
+        if linha:  # para evitar palavra vazia
+            palavras.add(linha)
+        if '-' in linha:
+            for palavra in linha.split('-'):
+                if palavra:
+                    palavras.add(palavra)  # para evitar palavra vazia
+
+    qt_original = i
+
+msg = '{} palavras na lista original, {} na lista gerada: {} adicionadas'
+extra = len(palavras) - qt_original
+print(msg.format(qt_original, len(palavras), extra))
+
+palavras = sorted(palavras, key=chave)
+
+with open(SAIDA, 'wt', encoding='utf-8') as saida:
+    saida.write('\n'.join(palavras))
+    saida.write('\n')
+
